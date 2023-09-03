a9dfaa94130805a5f837fd64e5255c3dd8576364
Bjørnar H. Langved <b.langved@icloud.com>
2023-09-03 18:26:16 +0200

    New pull request
    In my first attempt, I didnt make the branch for my own, but tried merging with the main branch. I deleted my pull request, and made a new fork (which is this).

---
+++ JS/validation/loginForm.js
@@ -0,0 +1,12 @@
+// function validateLoginForm() {
+//     const email = document.querySelector("email").value;
+//     const password = document.querySelector("password").value;
+
+//     if (password.length < 8) {
+//         alert("Password must be at least 8 characters long.");
+//         return false;
+//     }
+
+//     return true;
+// }
+
